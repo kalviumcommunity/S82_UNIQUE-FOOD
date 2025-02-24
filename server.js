97a7120bbd79e5a7e3ea1ea15d3f5f3cbd432c8b
uppala sadvika <uppalasadvika06@gmail.com>
2025-02-24 10:30:03 +0530

    sure

---
+++ server.js
@@ -0,0 +1,14 @@
+const express =require('express')
+require('dotenv').config()
+const cors = require('cors')
+const port=process.env.PORT
+
+app.use(cors())
+app.use(express.json())
+app.get('/',(req,res)=>{
+    res.status(400).send('hello world')
+})
+
+app.listen(port,()=>{
+    console.log(`server: connected successfully at ${port}`)
+})
